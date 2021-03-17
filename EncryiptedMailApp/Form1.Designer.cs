212ed556962ec86902daf4a4d14dd4f87f48083e
roalibiten <roalitarikbiten@gmail.com>
2021-03-17 15:26:18 +0300

    Initial commit

---
+++ EncryiptedMailApp/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace EncryiptedMailApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.mailFrom = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.message = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.subject = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.mailTo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.labelPath = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "E-Mail";
+            // 
+            // mailFrom
+            // 
+            this.mailFrom.Location = new System.Drawing.Point(38, 50);
+            this.mailFrom.Name = "mailFrom";
+            this.mailFrom.Size = new System.Drawing.Size(250, 22);
+            this.mailFrom.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 396);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "File";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // message
+            // 
+            this.message.Location = new System.Drawing.Point(38, 267);
+            this.message.Multiline = true;
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(250, 117);
+            this.message.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Message";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // subject
+            // 
+            this.subject.Location = new System.Drawing.Point(38, 213);
+            this.subject.Name = "subject";
+            this.subject.Size = new System.Drawing.Size(250, 22);
+            this.subject.TabIndex = 7;
+            this.subject.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Subject";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // mailTo
+            // 
+            this.mailTo.Location = new System.Drawing.Point(38, 158);
+            this.mailTo.Name = "mailTo";
+            this.mailTo.Size = new System.Drawing.Size(250, 22);
+            this.mailTo.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(35, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Send Mail Adress";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(38, 104);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(250, 22);
+            this.password.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(35, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 17);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Password";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(42, 417);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(76, 17);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Dosya Seç";
+            // 
+            // labelPath
+            // 
+            this.labelPath.AutoSize = true;
+            this.labelPath.Location = new System.Drawing.Point(125, 417);
+            this.labelPath.Name = "labelPath";
+            this.labelPath.Size = new System.Drawing.Size(70, 17);
+            this.labelPath.TabIndex = 13;
+            this.labelPath.Text = "dosyaismi";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 489);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 49);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 604);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelPath);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.mailTo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.subject);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mailFrom);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox mailFrom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox message;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox subject;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox mailTo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label labelPath;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EncryiptedMailApp/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace EncryiptedMailApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.From = new MailAddress(mailFrom.Text, "gethealthtip");
+                mailMessage.To.Add(new MailAddress(mailTo.Text));
+                mailMessage.Subject = subject.Text;
+                mailMessage.Body = message.Text;
+                mailMessage.IsBodyHtml = true;
+               /* if (labelPath.Text.Length > 0)
+                {
+                    if (System.IO.File.Exists(labelPath.Text))
+                    {
+                        mailMessage.Attachments.Add(new Attachment(labelPath.Text));
+
+                    }
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Credentials = new NetworkCredential(mailFrom.Text,password.Text);
+                    smtp.EnableSsl = true;
+                    smtp.Send(mailMessage);
+                    MessageBox.Show("Email Sent");
+
+                }*/
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential(mailFrom.Text, password.Text),
+
+                   // Credentials = new NetworkCredential(mailFrom.Text, password.Text),
+                    EnableSsl = true,
+                };
+                smtp.Send(mailFrom.Text, mailTo.Text, subject.Text, message.Text);
+
+                
+                MessageBox.Show("Email Sent");
+
+            }
+            catch 
+            {
+
+                MessageBox.Show("Please check your mail address and password.");
+
+            }
+        }
+    }
+}
